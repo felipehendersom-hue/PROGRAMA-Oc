485510b00ba4706da8a1308d849fbfa10542295d
felipehendersom-hue <felipehendersom@gmail.com>
2025-12-27 16:14:05 -0300

    mudanças do tema 2 para o 3

--- TRABALHO1.C/SUPER_TRUNFO1.c
+++ TRABALHO1.C/SUPER_TRUNFO1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 int main() {
     // Definição das variáveis para a Carta 1
     char estado[3], codigo_de_carta[5], nome_da_cidade[90];
@@ -15,6 +16,8 @@ float poder ;
     int pontos_turisticos2;
     float densidade_populacional2, pib_per_capita2;
 float poder2;
+
+int opcoes;
 
     // Cadastro da Carta 1
     printf("--- Digite os dados da primeira carta ---\n");
@@ -78,15 +81,103 @@ printf("Poder: %.2f\n", poder);
     printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacional2);
     printf("PIB per Capita: %.2f\n", pib_per_capita2);
 printf("Poder: %.2f\n", poder2);
-// comparação de tudo
 
-    printf(" POPULAÇÃO : CARTA 1 VENCEU (%d)\n", populacao > populacao2);
- printf(" PIB : CARTA 1 VENCEU (%d)\n", PIB > PIB2);
-    printf(" PONTOS TURÍSTICOS : CARTA 1 VENCEU (%d)\n", pontos_turisticos > pontos_turisticos2);
-    printf(" DENSIDADE POPULAÇÃO : CARTA 1 VENCEU (%d)\n", densidade_populacional < densidade_populacional2);
-    printf(" PIB PER CAPITA : CARTA 1 VENCEU (%d)\n", pib_per_capita > pib_per_capita2);
-    printf(" PODER : CARTA 1 VENCEU (%d)\n", poder > poder2);
+float v1_carta1, v1_carta2; // Valor do Atributo 1 (Carta 1 e 2)
+float v2_carta1, v2_carta2; // Valor do Atributo 2 (Carta 1 e 2)
 
 
+printf ("Escolha a categoria para comparar:\n") ;
+printf ("1 - POPULAÇÃO\n") ;
+printf ("2 - PIB\n") ;
+printf ("3 - PONTOS TURÍSTICOS\n") ;
+printf ("4 - DENSIDADE POPULAÇÃO\n") ;
+printf ("5 - PIB PER CAPITA\n") ;
+printf ("6 - PODER\n") ;
+scanf ("%d", &opcoes) ;
+
+ 
+switch (opcoes) {
+case 1 : v1_carta1 = (float)populacao ;
+v1_carta2 = (float)populacao2 ;
+break ;
+case 2 
+: v1_carta1 = PIB ;
+v1_carta2 = PIB2 ;
+break ;
+case 3 : v1_carta1 = (float)pontos_turisticos ;
+v1_carta2 = (float)pontos_turisticos2 ;
+break ;
+case 4 : v1_carta1 = -densidade_populacional ;
+v1_carta2 = -densidade_populacional2 ;
+
+break;
+case 5 : v1_carta1 = pib_per_capita ;
+v1_carta2 = pib_per_capita2 ;
+
+break;
+case 6 : 
+v1_carta1 = poder ;
+v1_carta2 = poder2 ;
+
+break;
+default:
+printf ("Opção inválida!\n") ;
+break;
+}
+int opcao2;
+printf (" Escolha o segundo atributo (sem repetição)\n") ;
+if (opcoes != 1) printf ("1 - POPULAÇÃO\n") ;
+if (opcoes != 2) printf ("2 - PIB\n") ;
+if (opcoes != 3) printf ("3 - PONTOS TURÍSTICOS\n") ;
+if (opcoes != 4) printf ("4 - DENSIDADE POPULAÇÃO\n") ;
+if (opcoes != 5) printf ("5 - PIB PER CAPITA\n") ;       
+if (opcoes != 6) printf ("6 - PODER\n") ;
+scanf ("%d", &opcao2) ;
+
+if (opcao2 == opcoes) {
+    printf (" Atributo repetido! \n") ;
+
+return 0;}
+
+switch (opcao2) {
+case 1 : v2_carta1 = (float)populacao ;
+v2_carta2 = (float)populacao2 ;
+break ;
+case 2 : v2_carta1 = PIB ;
+         v2_carta2 = PIB2 ;
+break ;
+case 3 : v2_carta1 = (float)pontos_turisticos ;
+         v2_carta2 = (float)pontos_turisticos2 ;
+break ;
+case 4 : v2_carta1 = -densidade_populacional ;
+         v2_carta2 = -densidade_populacional2 ;
+break;
+case 5 : v2_carta1 = pib_per_capita ;
+         v2_carta2 = pib_per_capita2 ;
+break;
+case 6 : v2_carta1 = poder ;
+         v2_carta2 = poder2 ;
+break;
+default:
+printf ("Opção inválida!\n") ;
+break;
+}
+
+float soma1, soma2;
+soma1 = v1_carta1 + v2_carta1 ;
+soma2 = v1_carta2 + v2_carta2 ;
+
+printf ("RESULTADOS DA COMPARAÇÃO:\n") ;
+printf (" cidade 1 %s  vs cidade 2 %s\n", nome_da_cidade, nome_da_cidade2) ;
+printf (" Soma dos atributos da cidade 1: %.2f\n", soma1) ;
+printf (" Soma dos atributos da cidade 2: %.2f\n", soma2) ;
+
+if (soma1 > soma2) {
+    printf (" A cidade %s é a vencedora!\n", nome_da_cidade) ;
+} else if (soma2 > soma1) {
+    printf (" A cidade %s é a vencedora!\n", nome_da_cidade2) ;
+} else {
+    printf (" Empate entre as cidades!\n") ;
+}
 return 0;
 } 
